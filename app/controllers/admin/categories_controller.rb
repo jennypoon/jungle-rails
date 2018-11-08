5f8aec7168114f879a29a8976a92131a17ffdcb5
Jenny Poon <jenny.poon@live.ca>
2018-11-08 01:17:14 +0000

    fixed typo so it id: :name

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,8 +1,10 @@
 class Admin::CategoriesController < ApplicationController
   def index
+    @categories = Category.order(id: :desc).all
   end
 
   def new
+    @category = Category.new
   end
 
   def create

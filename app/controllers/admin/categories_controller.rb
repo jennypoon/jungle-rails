2c6f1019bcb34f24b519d69d1b0aa3aca153563a
Jenny Poon <jenny.poon@live.ca>
2018-11-08 01:33:47 +0000

    added create method similar to product

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CategoriesController < ApplicationController
   def index
-    @categories = Category.order(id: :desc).all
+    @categories = Category.order(:name).all
   end
 
   def new
@@ -8,5 +8,20 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
+
+  private
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
 end

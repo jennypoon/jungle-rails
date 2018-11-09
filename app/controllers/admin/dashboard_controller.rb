8d7100e428df52d5de66c6aa0ddfd1c4ca61078d
Jenny Poon <jenny.poon@live.ca>
2018-11-09 22:47:03 +0000

    logged in user can write review and review shows up on page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,9 +1,8 @@
 class Admin::DashboardController < ApplicationController
 
-http_basic_authenticate_with name: ENV['AUTHENTICATION_USERNAME'], password: ENV['AUTHENTICATION_PASSWORD'], only: [:show]
-
-
+  http_basic_authenticate_with name: ENV['AUTHENTICATION_USERNAME'], password: ENV['AUTHENTICATION_PASSWORD'], only: [:show]
 
   def show
   end
+
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,19 @@
 class ReviewsController < ApplicationController
 
   def create
-    raise "Yay, I'm here!"
+    @review = Review.new
+    @review.product_id = params[:product_id]
+    @review.user = current_user
+    @review.description = params[:review][:description]
+    @review.rating = params[:review][:rating]
+
+    @review.save!
+
+      if @review.save
+        redirect_to @review.product
+      else
+        redirect_to @review.product
+      end
   end
 
 end

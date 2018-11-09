ab66155fdbcd21af7b3c943b3f542360e88a0ca3
Jenny Poon <jenny.poon@live.ca>
2018-11-09 21:18:31 +0000

    added form show page to submit reviews

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,9 +6,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
-    @review = Review.find_by(product_id: @product.id)
-    @user = User.find(@review.user_id)
-
+    @review = Review.where product_id: params[:id]
   end
 
 end

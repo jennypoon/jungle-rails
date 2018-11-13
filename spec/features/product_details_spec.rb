80844b58f1a4c675ebff5f495d41676f730b3867
Jenny Poon <jenny.poon@live.ca>
2018-11-13 20:18:32 +0000

    added tests for navigating to detail page

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see the detail page when click on image" do
+    # ACT
+    visit root_path
+
+    first(:css, 'img').click
+
+    # DEBUG
+    save_screenshot 'product-detail.png'
+
+    # VERIFY
+    expect(page).to have_text @category.products.first.name
+  end
+
+  scenario "They see the detail page when click on product name" do
+    # ACT
+    visit root_path
+
+    click_link(@category.products.first.name)
+
+    # DEBUG
+    sleep 2
+    save_screenshot 'product-detail.png'
+
+    # VERIFY
+    expect(page).to have_text @category.products.first.name
+
+  end
+
 end
